ad2e7174abd5bcb8efe1251925766c2d00a1b1a2
Programming Hero <programming.hero1@gmail.com>
2022-10-10 23:11:29 -0500

    connect with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home';
+import Phone from './components/Phone';
 
 function App() {
+  const router = createBrowserRouter([
+    { 
+      path: '/', 
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,
+          loader: () => fetch('http://localhost:5000/phones')
+        },
+        {
+          path: '/phones/:id',
+          element: <Phone></Phone>,
+          loader: ({params}) => fetch(`http://localhost:5000/phones/${params.id}`)
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
